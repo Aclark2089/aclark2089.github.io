2b8f4fb2a359023784593ffd27934789e67660fe
R. Alex Clark <aclark2089@gmail.com>
2017-07-23 14:12:49 -0400

    Setup new assets folder and updated gulpfile to inject dev and prod assets correctly

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/* 
+    R. Alex Clark
+*/
+
+(function($) {
+
+    // Site Settings
+    var settings = {};
+
+    $(function() {
+
+        // Setup variables
+        var $window = $(window),
+            $document = $(document),
+            $body = $('body'),
+            $html = $('html'),
+            $bodyHtml = $('bodyHtml'),
+            $wrapper = $('#wrapper');
+
+        // Setup loading class for body
+        $body.addClass('is-loading');
+
+    });
+
+})(jQuery);
--- gulpfile.js
+++ gulpfile.js
@@ -7,55 +7,69 @@ var inject = require('gulp-inject');
 var rs = require('run-sequence');
 
 // Compile sass to css
-gulp.task('sass', function () {
-    return gulp.src('./css/sass/**/*.scss')
+gulp.task('sass', function() {
+    return gulp.src('./assets/css/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./assets/css'));
 });
 
 // Watch for changes and recompile css
 gulp.task('watch', function() {
-    gulp.watch('css/sass/**/*.scss', ['sass:watch']);
+    gulp.watch('./assets/css/sass/**/*.scss', ['sass:watch']);
 });
 
 // Take updated css file and min / inject it
-gulp.task('sass:watch', function (cb) {
+gulp.task('sass:watch', function(cb) {
     rs('sass', 'minify-css', cb);
 });
 
 // Uglify JS
-gulp.task('uglify', function (cb) {
+gulp.task('uglify', function(cb) {
     pump([
-       gulp.src('js/**/*.js'),
-       uglify(),
-       gulp.dest('dist')
-   ],
-   cb
-   );
+            gulp.src('./assets/js/*.js'),
+            uglify(),
+            gulp.dest('./dist')
+        ],
+        cb
+    );
 });
 
 // Uglify CSS
 gulp.task('minify-css', function() {
-    return gulp.src('css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist'));
+    return gulp.src('./assets/css/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('./dist'));
 });
 
-// Inject dist
-gulp.task('inject', function () {
-   var target = gulp.src('./index.html');
-   var sources = gulp.src(['dist/*.js', 'dist/*.css'], {read: false});
+// Inject minified dist
+gulp.task('inject:prod', function() {
+    var target = gulp.src('./index.html');
+    var sources = gulp.src(['./dist/*.js', './dist/*.css'], { read: false });
 
-   return target.pipe(inject(sources))
-       .pipe(gulp.dest(''));
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest(''));
+});
+
+// Inject
+gulp.task('inject:dev', function() {
+    var target = gulp.src('./index.html');
+    var sources = gulp.src(['./assets/js/*.js', './assets/css/*.css'], { read: false });
+
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest(''));
 });
 
 // Compile sass, minify assets
-gulp.task('compile', ['sass'], function (cb) {
+gulp.task('compile', ['sass'], function(cb) {
     rs(['uglify', 'minify-css'], cb);
 });
 
-// Compile & inject into index.html
-gulp.task('prod', ['compile'], function (cb) {
-    rs(['inject']);
+// Compile & inject into index.html for production
+gulp.task('prod', ['compile'], function(cb) {
+    rs(['inject:prod']);
+});
+
+// Inject into index.html for dev
+gulp.task('dev', ['sass'], function(cb) {
+    rs(['inject:dev']);
 });
